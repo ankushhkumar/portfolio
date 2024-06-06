59857aa3c6bacd40db68b6df010cda20209ab578
ankushhkumar <ankushkumar7906@gmail.com>
2024-06-05 23:23:12 -0400

    About2.js linkedin link changed

--- src/components/About/About2.js
+++ src/components/About/About2.js
@@ -66,7 +66,7 @@ function About2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/ankushkumar7906/"
+                  href="https://www.linkedin.com/in/ankush7kumar/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
